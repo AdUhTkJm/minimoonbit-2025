6145dd215ef09968220225fd01eb315bbcd8d13e
Yue Huang <yh548@cam.ac.uk>
2025-08-07 10:34:31 +0800

    Analyze predecessor

--- scripts/pass-def-gen.py
+++ scripts/pass-def-gen.py
@@ -11,35 +11,50 @@ passes = data["passes"]
 print("// Auto generated. Do not edit!\n")
 
 for name in passes:
+  if isinstance(name, dict):
+    values = name
+    name = list(values.keys())[0];
+    fields: list[dict] = values[name]
+    names = list(list(entry.keys())[0] for entry in fields)
+    types = list(list(entry.values())[0][0] for entry in fields)
+    defaults = list(list(entry.values())[0][1] for entry in fields)
+
+    decl = "".join(f"\n  {name}: {type}" for name, type in zip(names, types))
+    default = "".join(f", {k}: {v}" for k, v in zip(names, defaults))
+  else:
+    decl = ""
+    default = ""
+
+
   Name = camel(name)
   print(textwrap.dedent(f"""
-    pub struct {Name} {{
-      module: Op
-    }}
+pub struct {Name} {{
+  module: Op{decl}
+}}
 
-    pub impl Pass for {Name} with name(_self) {{
-      return "{name}";
-    }}
+pub impl Pass for {Name} with name(_self) {{
+  return "{name}";
+}}
 
-    pub impl Pass for {Name} with operate(self) {{
-      {Name}::run(self);
-    }}
+pub impl Pass for {Name} with operate(self) {{
+  {Name}::run(self);
+}}
 
-    pub fn create_{name}(module: Op) -> {Name} {{
-      return {Name} :: {{ module }}
-    }}
+pub fn create_{name}(module: Op) -> {Name} {{
+  return {Name} :: {{ module{default} }}
+}}
   """), end='')
   # Generate some helper functions as well.
   print(textwrap.dedent(f"""
-    pub fn {Name}::funcs(self: {Name}) -> Array[Op] {{
-      let bb = self.module.region().block();
-      let result = [];
-      for op in bb {{
-        if (op.isa(Func)) {{
-          result.push(op);
-        }}
-      }}
-      return result;
+pub fn {Name}::funcs(self: {Name}) -> Array[Op] {{
+  let bb = self.module.region().block();
+  let result = [];
+  for op in bb {{
+    if (op.isa(Func)) {{
+      result.push(op);
     }}
+  }}
+  return result;
+}}
   """), end='')
 
